71dfc295a23d669725e9d3d1aeb9b1b2bcb89f73
muratcankirdar <muratcankirdar@gmail.com>
2023-04-28 14:09:36 +0300

    map exercise v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import { Button } from 'mui'
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  const names = ["Elif", "Murat", "Mert", "Hako"];
+  const people = [
+    {
+      name: "Elif",
+      age: 18,
+    },
+    {
+      name: "Murat",
+      age: 25,
+    },
+    {
+      name: "Mert",
+      age: 30,
+    },
+    {
+      name: "Hako",
+      age: 15,
+    },
+  ];
 
   return (
     <>
       <div>
-        {names.map((name, index) => {
+        {people.map((person) => {
           return (
             <>
-              <h2 key={name}>{index}</h2>
-              <button key={name}>{name}</button>
+              <h5>{`${person.name} ${person.age}`}</h5>
             </>
           );
         })}
-
-        {}
       </div>
     </>
   );
