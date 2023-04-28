3c620ac2ed680f1299042a5a6e9906ac352513bf
muratcankirdar <muratcankirdar@gmail.com>
2023-04-28 14:31:31 +0300

    exercise v4 - if else render, conditional props

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,34 @@ function App() {
     },
   ];
 
+  const age = 12;
+
+  // const namesStartsWithLetterM = people.filter((person) =>
+  //   person.name.startsWith("M")
+  // );
+
   return (
     <>
       <div>
         {/* {JSON.stringify(people)} */}
 
-        {people
-          .filter((person) => person.name.startsWith("M"))
-          .map((person) => (
-            <h2 key={person.name}>{person.name}</h2>
-          ))}
+        {people.map((person) => (
+          <h2
+            className={person.age > 23 ? "red-text" : "blue-text"}
+            style={{ textDecoration: person.age > 23 ? "underline" : "none" }}
+            key={person.name}
+          >
+            {person.name}
+          </h2>
+        ))}
+
+        {age > 18 ? (
+          <h2>18 yaşından büyüksünüz</h2>
+        ) : age === 18 ? (
+          <h2>18 yaşındasınız</h2>
+        ) : (
+          <h2>18 yaşından küçüksünüz</h2>
+        )}
       </div>
     </>
   );
